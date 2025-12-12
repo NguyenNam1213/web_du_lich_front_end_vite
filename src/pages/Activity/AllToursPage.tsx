e90e6d9d9ce12b487bb383abdf616752c23ee0fa
HaLT3103 <luongthaiha01@gmail.com>
2025-12-13 04:11:19 +0700

    feat(SubNavbar): add mega menu when hover button

--- src/pages/Activity/AllToursPage.tsx
+++ src/pages/Activity/AllToursPage.tsx
@@ -2,14 +2,33 @@ import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { RootState } from "../../store";
 import TourListItem from "../../components/Activity/TourListItem";
+import { useEffect, useState } from "react";
+import { getCities } from "../../services/api/cityApi";
+import { getDestinations } from "../../services/api/destinationApi";
 
 const AllToursPage = () => {
   const [searchParams] = useSearchParams();
   const tours = useSelector((state: RootState) => state.tour.tours);
 
-  const filter = searchParams.get("filter"); // featured | top-rated | sale...
-  const destinationId = searchParams.get("destination");
+  const filter = searchParams.get("filter");
+  const countryCode = searchParams.get("country");
+  const destinationId = searchParams.get("destinationId");
   const categoryId = searchParams.get("category");
+
+  const [cityMap, setCityMap] = useState<any[]>([]);
+  const [destMap, setDestMap] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const cities = await getCities();     
+      const destinations = await getDestinations();
+
+      setCityMap(cities);
+      setDestMap(destinations);
+    };
+
+    fetchData();
+  }, []);
 
   let filteredTours = [...tours];
 
@@ -32,12 +51,29 @@ const AllToursPage = () => {
       .sort((a, b) => (b as any).averageRating - (a as any).averageRating);
   }
 
+  // FILTER COUNTRY
+  if (countryCode && cityMap.length > 0 && destMap.length > 0) {
+    const cityIds = cityMap
+      .filter(c => c.countryCode === countryCode)
+      .map(c => Number(c.id));
+
+    const destinationIds = destMap
+      .filter(d => cityIds.includes(Number(d.cityId)))
+      .map(d => Number(d.id));
+
+    filteredTours = filteredTours.filter(t =>
+      destinationIds.includes(Number(t.destinationId))
+    );
+  }
+
+  // FILTER DESTINATION
   if (destinationId) {
     filteredTours = filteredTours.filter(
       t => String(t.destinationId) === destinationId
     );
   }
 
+  // FILTER CATEGORY
   if (categoryId) {
     filteredTours = filteredTours.filter(
       t => String(t.categoryId) === categoryId
